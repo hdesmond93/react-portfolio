cfda4ba03ce5290e0b574162e0042d77048c3aa4
Heaven <hdesmond93@gmail.com>
2021-01-27 11:51:20 -0600

    Header
    Added header and particles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#8bc34a"
+              }
+            },
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#3ca9d1",
+                blur: 5
+              }
+            },
+            collisions: {
+              enable: true,
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
